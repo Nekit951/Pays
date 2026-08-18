1929a8960c745dfb43ddaad79826ca44d61ffc55
Nekit951 <Mdvdvnkt@gmail.com>
2026-08-18 13:07:10 +0300

    Update OrderModel.kt

--- OrderModel.kt
+++ OrderModel.kt
@@ -4,5 +4,5 @@ import com.google.firebase.database.IgnoreExtraProperties
 import java.io.Serializable
 
 @IgnoreExtraProperties
-data class OrderModel(val id: String = "", val items: ArrayList<ItemsModel> = ArrayList(), val summa: Double = 0.0, val quantity: Int = 0, val address: String = "", val paymentToken: String = "",
+data class OrderModel(val id: String = "", val items: ArrayList<ItemsModel> = ArrayList(), val user: UserModel? = null, val summa: Double = 0.0, val quantity: Int = 0, val address: String = "", val paymentToken: String = "",
                       val status: String = "pending"): Serializable
