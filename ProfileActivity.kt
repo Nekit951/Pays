89213d84c3b2c5d254652fc5e8e30edb7c7b8913
Nekit951 <Mdvdvnkt@gmail.com>
2026-08-18 13:14:00 +0300

    Update ProfileActivity.kt

--- ProfileActivity.kt
+++ ProfileActivity.kt
@@ -1,13 +1,13 @@
 package com.example.pays
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -23,6 +23,9 @@ class ProfileActivity : AppCompatActivity() {
         val name: TextView = findViewById(R.id.fio_profile)
         val phone: TextView = findViewById(R.id.phone_profile)
         val email: TextView = findViewById(R.id.email_profile)
+        val btnEdit: ImageView = findViewById(R.id.buttonOptions)
+        val btnExit: Button = findViewById(R.id.exit)
+        val sharePref = getSharedPreferences("UserPref", MODE_PRIVATE)
         val firebase = FirebaseDatabase.getInstance().getReference("User")
         val userId = intent.getStringExtra("USER_ID") ?: ""
 
@@ -35,7 +38,7 @@ class ProfileActivity : AppCompatActivity() {
                     email.text = user?.email  ?: "Имя не указано"
                 }
                 else{
-                    Toast.makeText(applicationContext, "Пользователь не найден", Toast.LENGTH_SHORT).show()
+                    register()
                 }
             }
 
@@ -43,5 +46,33 @@ class ProfileActivity : AppCompatActivity() {
                 Log.e("FIREBASE_DEBUG", "Ошибка: ${error.message}")
             }
         })
+
+        btnExit.setOnClickListener {
+            val editor = sharePref.edit()
+            editor.clear()
+            editor.apply()
+
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnEdit.setOnClickListener {
+            val intent = Intent(this, ProfileEditActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun register(){
+        val sharePref = getSharedPreferences("UserPref", MODE_PRIVATE)
+        with(sharePref.edit()) {
+            putBoolean("isLoggedIn", false)
+            putString("USER_ID", "")
+            apply()
+        }
+
+        val intent = Intent(this, RegActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
