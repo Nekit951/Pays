357f61d413eb9df219ed8507ddb06af834ffc78f
Nekit951 <Mdvdvnkt@gmail.com>
2026-08-19 12:10:38 +0300

    Delete OrderModel.kt

--- OrderModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pays
-
-import com.google.firebase.database.IgnoreExtraProperties
-import java.io.Serializable
-
-@IgnoreExtraProperties
-data class OrderModel(val id: String = "", val items: ArrayList<ItemsModel> = ArrayList(), val user: UserModel? = null, val summa: Double = 0.0, val quantity: Int = 0, val address: String = "", val paymentToken: String = "",
-                      val status: String = "pending"): Serializable
