c73db3b0979fefc97d2e3c03894c39c545d47ca8
Nekit951 <Mdvdvnkt@gmail.com>
2026-08-19 12:08:27 +0300

    Delete ItemsModel.kt

--- ItemsModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pays
-
-import java.io.Serializable
-
-data class ItemsModel(val id: Int = 0, val title: String = "", val price: Double = 0.0, val quantity: Int = 0, val picUrl: String = "", var numberInCart: Int = 1): Serializable
