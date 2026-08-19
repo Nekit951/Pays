81f2cef315046dde072be503448b02a693c36206
Nekit951 <Mdvdvnkt@gmail.com>
2026-08-19 12:12:10 +0300

    Delete UserModel.kt

--- UserModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pays
-
-import com.google.firebase.database.IgnoreExtraProperties
-import java.io.Serializable
-
-@IgnoreExtraProperties
-data class UserModel(val id: String = "", val name: String = "", val address: UserAddress? = null, val phone: String = "", val email: String = ""): Serializable
