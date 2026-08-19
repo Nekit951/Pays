a7eebbc24cbbcc1d46f74d0c4513b389b710da9f
Nekit951 <Mdvdvnkt@gmail.com>
2026-08-19 12:12:01 +0300

    Delete UserAddress.kt

--- UserAddress.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pays
-
-import com.google.firebase.database.IgnoreExtraProperties
-
-@IgnoreExtraProperties
-data class UserAddress(val city: String = "", val podiezd: String = "", val etazh: String = "", val kvartira: String = "")
